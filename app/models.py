c5271d7888e103272a6c62b8c7fbbebf79faba00
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-12-02 18:49:17 +0530

    create simple schema

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class FB_Comment(models.Model):
+    comment_id = models.CharField(max_length=20, primary_key=True)
+    post_id = models.CharField(max_length=20)
+    page_id = models.CharField(max_length=20)
+    message = models.CharField(max_length=20)
+    sender_name = models.CharField(max_length=20)
+    sender_id = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} | {}'.format(self.sender_name, self.comment_id)
+
+class TW_Tweet(models.Model):
+    pass
+
+class Issue(models.Model):
+    source = models.CharField(max_length=20)
+    priority = models.IntegerField(default=0)
+    comment = models.ForeignKey(FB_Comment, null=True, blank=True)
+    tweet = models.ForeignKey(TW_Tweet, null=True, blank=True)
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.source, self.comment, self.tweet)
+
+
+class Conversation(models.Model):
+    # comment = models.ForeignKey(FB_Comment)
+    retweet = models.ForeignKey(TW_Tweet, null=True, blank=True)
+    issue = models.ForeignKey(Issue, null=True, blank=True)
